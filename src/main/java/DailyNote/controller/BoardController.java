8bf4a5af96060ddc4f03a85257fa3c5a377bfa0d
ryudain05 <dkdlsekdls@naver.com>
2023-10-06 17:48:50 +0900

    Feat/save.html save.html 추가

--- src/main/java/DailyNote/controller/BoardController.java
+++ src/main/java/DailyNote/controller/BoardController.java
@@ -3,10 +3,12 @@ package DailyNote.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/board")
 public class BoardController {
-    @GetMapping("/board/save")
+    @GetMapping("/save")
     public String saveForm() {
         return "save";
     }
