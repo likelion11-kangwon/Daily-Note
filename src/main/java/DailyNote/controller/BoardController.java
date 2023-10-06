77fc80ebd64c063486835e8728da013811473ae9
ryudain05 <dkdlsekdls@naver.com>
2023-10-06 17:47:05 +0900

    Feat: HomeController, BoardController Controller 생성 -> GetMapping만 추가

---
+++ src/main/java/DailyNote/controller/BoardController.java
@@ -0,0 +1,14 @@
+package DailyNote.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+    @GetMapping("/board/save")
+    public String saveForm() {
+        return "save";
+    }
+
+}
